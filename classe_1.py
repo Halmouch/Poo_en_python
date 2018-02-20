d55accd987c45970b08143ca34e72ed00e7fb961
Helmighanmi <massaoudghanmi@yahoo.com>
2018-02-20 22:19:32 +0100

    Creation du premier class en python nommee voiture

---
+++ classe_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+#c'est un classe saimple en python 
+
+class Voiture:
+
+	# definir un constructeur 
+	def __init__(self):
+		self.nom = "Audi "
+
+	#definir une methode 
+	def donne_moi_le_modele(self):
+		return "250"
